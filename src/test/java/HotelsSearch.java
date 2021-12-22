cf073c2c5201ca95a8c118378a28b64325607dce
elhispanopolako <tadeo98hiszpan@gmail.com>
2021-12-22 18:24:03 +0100

    set checkout by calendar

--- src/test/java/HotelsSearch.java
+++ src/test/java/HotelsSearch.java
@@ -17,7 +17,10 @@ public class HotelsSearch {
         driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
-        driver.findElement(By.name("checkin")).sendKeys("17/04/2022");
-        driver.findElement(By.name("checkout")).sendKeys("22/04/2022");
+        driver.findElement(By.name("checkin")).sendKeys("26/12/2021");
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
+                .stream().filter(el -> el.isDisplayed())
+                .findFirst().ifPresent(el ->el.click());
     }
 }
