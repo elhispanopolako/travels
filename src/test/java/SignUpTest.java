61efbce7dd3738d3f21167b27ba508e3f4d7f8cf
elhispanopolako <tadeo98hiszpan@gmail.com>
2021-12-23 00:07:38 +0100

    Set signup

---
+++ src/test/java/SignUpTest.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class SignUpTest {
+    @Test
+    public void signUp() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().filter(WebElement::isDisplayed)
+                .findFirst().ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+        String lastname="Woźniak";
+
+        driver.findElement(By.name("firstname")).sendKeys("Tadeusz");
+        driver.findElement(By.name("lastname")).sendKeys(lastname);
+        driver.findElement(By.name("phone")).sendKeys("792159500");
+        driver.findElement(By.name("email")).sendKeys("tadeo981tester@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("test123");
+        driver.findElement(By.name("confirmpassword")).sendKeys("test123");
+        driver.findElements(By.xpath("//button[@type='submit']")).get(0).click();
+
+        WebElement header= driver.findElement(By.xpath("//h3[@class='RTL']"));
+
+        Assert.assertTrue(header.getText().contains(lastname));
+
+    }
+
+}
